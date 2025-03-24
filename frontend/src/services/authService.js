da1569a4182806bd677083730c1cd62c8f1328fa
Shreyam Hemanta <shreyam2014@gmail.com>
2025-03-25 01:11:32 +0530

    commit

--- frontend/src/services/authService.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-
-export const login = (data) => axios.post("/api/auth/login", data);
-export const register = (data) => axios.post("/api/auth/register", data);
--- frontend/src/services/eventService.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-
-export const getEvents = () => axios.get("/api/events").then(res => res.data);
-export const getEventById = (id) => axios.get(`/api/events/${id}`).then(res => res.data);
