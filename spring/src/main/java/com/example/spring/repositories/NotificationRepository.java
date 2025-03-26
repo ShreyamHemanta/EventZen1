2cfb8e5cf075e3b5dc2404d34d9fe14ca0126275
Shreyam Hemanta <shreyam2014@gmail.com>
2025-03-26 14:44:20 +0530

    commit

--- spring/src/main/java/com/example/spring/repositories/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring.repositories;
-
-import com.example.spring.models.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
--- spring/src/main/java/com/example/spring/repositories/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring.repositories;
-
-import com.example.spring.models.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
