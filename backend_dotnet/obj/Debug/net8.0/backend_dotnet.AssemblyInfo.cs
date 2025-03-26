47e074b305c9a5c5d8c467a51216d6f8393c8c3b
Shreyam Hemanta <shreyam2014@gmail.com>
2025-03-26 23:49:13 +0530

    commit

--- backend_dotnet/obj/Debug/net8.0/backend_dotnet.AssemblyInfo.cs
+++ backend_dotnet/obj/Debug/net8.0/backend_dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4f5c7a9534b524afc111cfb37c0d83cf41b034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cfb8e5cf075e3b5dc2404d34d9fe14ca0126275")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- eventzen-frontend/vite.config.js
+++ eventzen-frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true,
+    historyApiFallback: true, // Fix for MIME type issue
+  },
+});
